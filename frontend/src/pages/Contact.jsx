3b9c9de597b385b0fe1cbdedfd2ace621cdc8201
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2023-01-31 12:58:20 +0530

    create about and contact pages

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col } from "reactstrap";
+
+const Contact = () => {
+  return (
+    <section className="contact">
+      <Container>
+        <Row>
+          <Col lg="4">Contact</Col>
+          <Col lg="8">Contact</Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
+
+export default Contact
