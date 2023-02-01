a63e70aed9d0395ea61438022eb39d153b2d14f0
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2023-02-01 23:33:29 +0530

    add about page

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+const About = () => {
+  return (
+    <section className="about">
+      <Container>
+        <Row>
+          <Col lg="12">Hello</Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
