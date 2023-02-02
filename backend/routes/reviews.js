16eccb1b64b363f9edf49c82c07ed96f378ead6b
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2023-02-03 03:09:00 +0530

    add login register and review routes

---
+++ backend/routes/reviews.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { createReview } from "../controllers/reviewController.js";
+
+const router = express.Router();
+
+router.post("/:tourId", createReview);
+export default router;
