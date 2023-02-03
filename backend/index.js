f73f4bbc889fb13e00edf9a1bbc5126b9d67280a
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2023-02-04 02:19:52 +0530

    add booking route api

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ import authRoute from "./routes/auth.js";
 import tourRoute from "./routes/tours.js";
 import userRoute from "./routes/users.js";
 import reviewRoute from "./routes/reviews.js";
+import bookingRoute from "./routes/bookings.js";
+
 
 dotenv.config();
 const app = express();
@@ -41,6 +43,8 @@ app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/tours", tourRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/review", reviewRoute);
+app.use("/api/v1/booking", bookingRoute);
+
 
 // app running port
 app.listen(port, () => {
