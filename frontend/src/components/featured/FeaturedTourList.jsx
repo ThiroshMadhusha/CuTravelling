97de1499580c578ac7edf5d139d991e631bbd65e
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2023-02-05 01:12:59 +0530

    update featured tour list

--- frontend/src/components/featured/FeaturedTourList.jsx
+++ frontend/src/components/featured/FeaturedTourList.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import TourCard from "../../shared/TourCard";
-import tourData from "../../assets/data/tours";
+// import tourData from "../../assets/data/tours";
 import { Col } from "reactstrap";
 
+import useFetch from "../../hooks/useFetch";
+import { BASE_URL } from "../../utils/config";
+
 const FeaturedTourList = () => {
+  const {
+    data: featuredTours,
+    loading,
+    error,
+  } = useFetch(`${BASE_URL}/tours/search/getFeaturedTours`);
+
+  console.log(featuredTours);
   return (
     <>
-      {tourData?.map((tour) => (
-        <Col lg="3" className="mb-4" key={tour.id}>
-          <TourCard tour={tour} />
-        </Col>
-      ))}
+      {loading && <h4>Loading.......</h4>}
+      {error && <h4>{error}</h4>}
+      {/* {tourData?.map((tour) => (
+       */}
+      {!loading &&
+        !error &&
+        featuredTours?.map((tour) => (
+          <Col lg="3" className="mb-4" key={tour._id}>
+            <TourCard tour={tour} />
+          </Col>
+        ))}
     </>
   );
 };
